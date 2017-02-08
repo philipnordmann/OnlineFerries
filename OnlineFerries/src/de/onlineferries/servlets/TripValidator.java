75b88616fb29d14c10fd0eb1d5a6aa8ab2acd99e
Philip <Philip@PHILIP-PC.fritz.box>
2017-02-08 22:59:50 +0100

    aa

--- OnlineFerries/src/de/onlineferries/servlets/TripValidator.java
+++ OnlineFerries/src/de/onlineferries/servlets/TripValidator.java
@@ -1,11 +1,16 @@
 package de.onlineferries.servlets;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import de.onlineferries.entity.Trip;
+import de.onlineferries.model.service.EntityManagerFactoryService;
 
 /**
  * Servlet implementation class TripValidator
@@ -18,15 +23,24 @@ public class TripValidator extends HttpServlet {
      * Default constructor. 
      */
     public TripValidator() {
-        // TODO Auto-generated constructor stub
+        
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String validator = request.getParameter("validator");
+		
+		boolean available = false;
+		
+		if (validator.equals("cabin")) {
+			available = validateCabin(Integer.parseInt(request.getParameter("cabin")), 1, 2);
+		} else if(validator.equals("passanger")) {
+			available = validatePassenger(0);
+		}
+		
+		response.getWriter().print(available);
 	}
 
 	/**
@@ -35,6 +49,21 @@ public class TripValidator extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	private boolean validateCabin(int cabinId, int tripId, int count){
+		EntityManager em = EntityManagerFactoryService.getEntityManagerFactory().createEntityManager();
+		
+		Trip t = em.find(Trip.class, tripId);
+		int cabinCount = t.getRoute().getShip().getShipCabin().size();
+		long reserved = t.getReservations().stream().mapToLong((r) -> r.getCabins().stream().filter(c -> c.getCabin().getId() == cabinId).count()).sum();
+		
+		return reserved + count < cabinCount;
+		
+		}
+	
+	private boolean validatePassenger(int i){
+		return false;
 	}
 
 }
